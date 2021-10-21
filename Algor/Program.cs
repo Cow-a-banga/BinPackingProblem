54d0e25664d4c9e2c01f88269dbd88d1ce5f1bfd
Dmitry <chiruhind@gmail.com>
2021-10-21 15:20:00 +0500

    Initial commit

---
+++ Algor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Algor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TestGenerator generator = new TestGenerator();
+
+            var input = generator.Generate(10, 50);
+
+            var alg = new BruteForceAlgorithm();
+            var res = alg.Pack(input);
+            
+            Console.WriteLine(string.Join("\n", input.Things.Select(t=>$"{t.id}  {t.Mass}")));
+            Console.WriteLine();
+            Console.WriteLine(string.Join("\n", res.ThingsInContainers.Select(c => string.Join(" ", c))));
+        }
+    }
+}
---
+++ Algor/TestGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Algor
+{
+    public class TestGenerator
+    {
+        public IInputData Generate(int thingsCount, int containersCapacity)
+        {
+            var result = new InputData
+            {
+                ThingsNum = thingsCount,
+                ContainersCapacity = containersCapacity,
+                Things = new IThing[thingsCount]
+            };
+            
+            Random random = new Random();
+
+            for (int i = 0; i < thingsCount; i++)
+            {
+                int mass = random.Next(1, containersCapacity / 2);
+                result.Things[i] = new Thing(i + 1, mass);
+            }
+
+            return result;
+        }
+    }
+}
